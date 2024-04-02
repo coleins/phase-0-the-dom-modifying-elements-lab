e33d70d4b9f4b3f2f3f30d425d8aa5aadfafc9b1
Collins Ngetich <collinsngetichk@gmail.com>
2024-04-02 07:49:52 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const element = document.querySelector("#main");
+element.remove();
+
+// Create an <h1> element
+const newHeader = document.createElement("h1");
+
+// Set the id attribute of the <h1> element to 'victory'
+newHeader.id = "victory";
+
+// Append the <h1> element to the document body (or another element)
+document.body.appendChild(newHeader);
